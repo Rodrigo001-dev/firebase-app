be64303d2662783011d5f3c286de29080a498dbe
Rodrigo <rodrigorael53@gmail.com>
2022-05-02 12:52:16 -0300

    redirecting the user if he is authenticated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from '@react-navigation/native';
+
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
 import { Home } from '@screens/Home';
 import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+  useEffect(() => {
+    // onAuthStateChanged vai ficar observando quando muda o estado do usuário
+    const subscriber = auth().onAuthStateChanged(setUser);
+
+    return subscriber;
+  }, []);
+
   return (
     <NavigationContainer>
-      <AuthRoutes />
+      {user ? <Home /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }
